016e86d19ca30a077c1a0f5448dda73e9d98b07c
aitzaz21 <aitzazanwar1@icloud.com>
2026-04-24 01:55:41 +0500

    Fix Nav Bar

--- client/src/modules/home/components/Navbar.jsx
+++ client/src/modules/home/components/Navbar.jsx
@@ -152,8 +152,8 @@ export default function Navbar() {
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" />
               </svg>
             </div>
-            <span className="font-display font-bold text-lg tracking-wider text-white group-hover:text-white transition-colors">
-              INKIFY
+            <span className="font-display font-bold text-lg tracking-wide text-white group-hover:text-white transition-colors">
+              Inkify Printing
             </span>
           </Link>
 
